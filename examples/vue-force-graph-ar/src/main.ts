f328b47a39525566533d88385a0c2f17c3a9f224
zjfcool <2568596196@qq.com>
2026-06-03 21:20:06 +0800

    docs: update ar example

--- examples/vue-force-graph-ar/src/main.ts
+++ examples/vue-force-graph-ar/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import "@ar-js-org/ar.js";
+// import "@ar-js-org/ar.js";
 import App from "./App.vue";
 import ForceGraphAR from "vue-force-graph-ar";
 import router from "@/routes";
