48351e30a320e1de57c9652f99970cabb7847239
zjfcool <2568596196@qq.com>
2026-06-05 10:50:26 +0800

    docs: update ar example

--- examples/vue-force-graph-ar/src/views/basic.vue
+++ examples/vue-force-graph-ar/src/views/basic.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <ForceGraphAR
     :enable-resize="false"
     :graph-data="graphData"
@@ -21,4 +21,34 @@ const graphData = {
       target: Math.round(Math.random() * (id - 1)),
     })),
 };
+</script> -->
+<template>
+  <ForceGraphAR ref="graph" :enable-resize="false"></ForceGraphAR>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import type { ForceGraphARExpose } from "vue-force-graph-ar";
+const graph = ref<ForceGraphARExpose | null>(null);
+// Random tree
+const N = 200;
+const gData = {
+  nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+  links: [...Array(N).keys()]
+    .filter((id) => id)
+    .map((id) => ({
+      source: id,
+      target: Math.round(Math.random() * (id - 1)),
+    })),
+};
+
+onMounted(() => {
+  graph.value
+    ?.graphData(gData)
+    .nodeRelSize(10)
+    .linkWidth(3)
+    .nodeOpacity(0.9)
+    .linkOpacity(0.4)
+    .nodeColor(() => "firebrick")
+    .linkColor(() => "darkgrey");
+});
 </script>
